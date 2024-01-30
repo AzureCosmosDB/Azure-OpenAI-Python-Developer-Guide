85128f9e962ce5aeb19debbd8e34e7cea7ff7e67
Chris Pietschmann <chris@build5nines.com>
2024-01-30 14:27:13 -0500

    update pandoc script

--- run-pandoc.sh
+++ run-pandoc.sh
@@ -10,9 +10,7 @@
 pandoc -f markdown --toc \
     -t docx \
     -o "cosmos-db-openai-python-dev-guide.docx" \
-    # --resource-path $IMAGEFOLDERS \
-    # -s $MARKDOWNFILES 
-    --resource-path "./00_Introduction":"./01_Azure_Overview":"./02_Overview_Cosmos_DB":"./03_Overview_Azure_OpenAI":"./04_Overview_AI_Concepts":"./05_Explore_OpenAI_models":"./06_Provision_Azure_Resources":"./07_Create_First_Cosmos_DB_Project":"./08_Load_Data":"./09_Vector_Search_Cosmos_DB":"./10_LangChain":"./11_Backend_API":"./12_User_Interface"
+    --resource-path "./00_Introduction":"./01_Azure_Overview":"./02_Overview_Cosmos_DB":"./03_Overview_Azure_OpenAI":"./04_Overview_AI_Concepts":"./05_Explore_OpenAI_models":"./06_Provision_Azure_Resources":"./07_Create_First_Cosmos_DB_Project":"./08_Load_Data":"./09_Vector_Search_Cosmos_DB":"./10_LangChain":"./11_Backend_API":"./12_User_Interface" \
     -s "./00_Introduction/README.md" \
     "./01_Azure_Overview/README.md" \
     "./02_Overview_Cosmos_DB/README.md" \
