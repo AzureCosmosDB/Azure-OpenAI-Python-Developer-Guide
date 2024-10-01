f76005f0c16b3cd471ed3f17df5513776483e2fd
Chris Pietschmann <chris@build5nines.com>
2024-10-01 15:12:13 -0400

    working on session state

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,8 @@ API entrypoint for backend API.
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import uuid
+
 from api_models.ai_request import AIRequest
 from cosmic_works.cosmic_works_ai_agent import CosmicWorksAIAgent
 
@@ -38,6 +40,16 @@ def run_cosmic_works_ai_agent(request: AIRequest):
     """
     Run the Cosmic Works AI agent.
     """
-    if request.session_id not in agent_pool:
-        agent_pool[request.session_id] = CosmicWorksAIAgent(request.session_id)
-    return { "message": agent_pool[request.session_id].run(request.prompt) }
+    prompt = request.prompt
+    session_id = request.session_id
+
+    # If no session_id is provided or default is provided, generate a new one.
+    if (session_id is None or session_id == "1234"):
+        session_id = str(uuid.uuid4())
+
+    # If the session_id is not in the agent pool, create a new agent.
+    if session_id not in agent_pool:
+        agent_pool[session_id] = CosmicWorksAIAgent(session_id)
+
+    # Run the agent with the provided prompt.
+    return { "message": agent_pool[session_id].run(prompt), "session_id": session_id }
